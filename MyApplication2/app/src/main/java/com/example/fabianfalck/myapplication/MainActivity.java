caee36cf94a11b7e7970bbb781e65d380a358c58
FabianFalck <fabian.falck@web.de>
2016-10-24 23:50:46 +0800

    Tut9

--- MyApplication2/app/src/main/java/com/example/fabianfalck/myapplication/MainActivity.java
+++ MyApplication2/app/src/main/java/com/example/fabianfalck/myapplication/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.fabianfalck.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText pass_word;
+    private Button button;
+
     public static final String MY_TAG = "my custom message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,4 +49,22 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         Log.d("lifecycle","onDestroy invoked");
     }
+
+    public void onButtonClick(View v){ //Import fixed with alt+enter
+        EditText e1 = (EditText)findViewById(R.id.editText3);
+        EditText e2 = (EditText)findViewById(R.id.editText4);
+        TextView t1 = (TextView)findViewById(R.id.textView7);
+        int num1 = Integer.parseInt(e1.getText().toString());
+        int num2 = Integer.parseInt(e2.getText().toString());
+
+        int sum = num1 + num2;
+
+        t1.setText(Integer.toString(sum));
+
+    }
+
+    public void addListenerOnButton(){
+        pass_word = (EditText) findViewById(R.id.editText2);
+    }
+
 }
