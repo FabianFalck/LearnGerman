ce1b2e22be3021de20be4a4c327b571932df9316
FabianFalck <fabian.falck@web.de>
2016-11-16 12:33:03 +0800

    first app

--- Prototype2/app/src/main/java/com/example/fabianfalck/prototype2/MainActivity.java
+++ Prototype2/app/src/main/java/com/example/fabianfalck/prototype2/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.fabianfalck.prototype2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.fabianfalck.prototype2.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message); //dont we just pass a string here? why key value pair?
+        startActivity(intent);
+    }
+
 }
